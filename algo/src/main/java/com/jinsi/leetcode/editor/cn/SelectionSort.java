0b26c91dd1f54bd6dd541e9dc1fd3c0cdb7f6c96
zhaoyangyu <zhaoyangyu@xiaohongshu.com>
2024-06-07 10:58:41 +0800

    选择排序

--- algo/src/main/java/com/jinsi/leetcode/editor/cn/SelectionSort.java
+++ algo/src/main/java/com/jinsi/leetcode/editor/cn/SelectionSort.java
@@ -8,7 +8,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         Solution solution = new SelectionSort().new Solution();
         int[] ints = {3, 2, 4, 10, 1};
-        solution.sort(ints);
+        solution.sort2(ints);
         System.out.println(Arrays.toString(ints));
     }
 
@@ -27,6 +27,44 @@ public class SelectionSort {
                     }
                 }
                 nums[i] = minValue;
+            }
+        }
+
+        public void sort1(int[] nums) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                // 最小元素的索引
+                int minIndex = i;
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] < nums[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex == i) {
+                    continue;
+                }
+                // 交换元素位置
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+
+        public void sort2(int[] nums) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                // 最大元素的索引
+                int maxIndex = i;
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] > nums[maxIndex]) {
+                        maxIndex = j;
+                    }
+                }
+                if (maxIndex == i) {
+                    continue;
+                }
+                // 交换元素位置
+                int temp = nums[i];
+                nums[i] = nums[maxIndex];
+                nums[maxIndex] = temp;
             }
         }
     }
