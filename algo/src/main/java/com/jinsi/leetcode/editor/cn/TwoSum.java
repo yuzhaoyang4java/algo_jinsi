eb0fa54fc563343ccd0c0a181d4360d564bf6122
zhaoyangyu <zhaoyangyu@xiaohongshu.com>
2024-06-07 15:59:07 +0800

    init

--- algo/src/main/java/com/jinsi/leetcode/editor/cn/TwoSum.java
+++ algo/src/main/java/com/jinsi/leetcode/editor/cn/TwoSum.java
@@ -1,65 +1,73 @@
 /**
-给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
-
- 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
-
- 你可以按任意顺序返回答案。
-
-
-
- 示例 1：
-
-
-输入：nums = [2,7,11,15], target = 9
-输出：[0,1]
-解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
-
-
- 示例 2：
-
-
-输入：nums = [3,2,4], target = 6
-输出：[1,2]
-
-
- 示例 3：
-
-
-输入：nums = [3,3], target = 6
-输出：[0,1]
-
-
-
-
- 提示：
-
-
- 2 <= nums.length <= 10⁴
- -10⁹ <= nums[i] <= 10⁹
- -10⁹ <= target <= 10⁹
- 只会存在一个有效答案
-
-
-
-
- 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？
-
- Related Topics 数组 哈希表
- 👍 18608 👎 0
-
-*/
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 2 <= nums.length <= 10⁴
+ * -10⁹ <= nums[i] <= 10⁹
+ * -10⁹ <= target <= 10⁹
+ * 只会存在一个有效答案
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？
+ * <p>
+ * Related Topics 数组 哈希表
+ * 👍 18608 👎 0
+ */
 
 package com.jinsi.leetcode.editor.cn;
-public class TwoSum{
-   public static void main(String[] args) {
-         Solution solution = new TwoSum().new Solution();
-   }
-   //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        return null;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
